5e31d2158feb4370b5f06306e28fd249210ce77a
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-02-17 18:55:46 +0900

    filter

--- level_1/filter/filter.c
+++ level_1/filter/filter.c
@@ -1,12 +1,5 @@
-
-
-/*
-大容量malloc -> readで全データappend
--> memmemで検索しながら置換 -> writeで出力
-*/
-
 #define _GNU_SOURCE
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 2
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
@@ -14,35 +7,52 @@
 #include <stdlib.h>
 
 int main(int ac, char **av) {
-	if (ac != 2)
+	if (ac != 2 | strlen(av[1]) == 0)
 		return (1);
 
-	char *search_start = malloc(BUFFER_SIZE);
-	if (!search_start)
-		return (perror("Error: "), free(search_start), 1);
-	char *result = search_start;
+	char *buffer_start = malloc(BUFFER_SIZE);
+	if (!buffer_start)
+		return (perror("Error"), free(buffer_start), 1);
 
-	ssize_t bytes_read = read(0, search_start, BUFFER_SIZE);
-	if (bytes_read == -1)
-		return (perror("Error: "), free(search_start), 1);
-	search_start[bytes_read] = '\0';
-	size_t search_len = strlen(search_start);
+	ssize_t bytes_read;
+	size_t used_size = 0;
+	char *new_buffer;
+
+	while (1) {
+		// readで読み取る前に、今まで読み込んだbufferの量がBUFFER_SIZEを超えていたら、reallocでバッファサイズを増やす
+		if (used_size + BUFFER_SIZE > BUFFER_SIZE) {
+			new_buffer = realloc(buffer_start, used_size + BUFFER_SIZE);
+			if (!new_buffer)
+				return (perror("Error"), free(buffer_start), 1);
+			buffer_start = new_buffer;
+		}
+
+		bytes_read = read(0,  buffer_start + used_size, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (perror("Error"), free(buffer_start), 1);
+		if (bytes_read == 0) break;
+
+		used_size += bytes_read;
+	}
+
+	buffer_start[used_size] = '\0';
+	size_t buffer_len = strlen(buffer_start);
 
 	char *pattern = av[1];
 	size_t pattern_len = strlen(pattern);
 
 	// memmemで検索しながら置換を繰り返す
-	// search_startは最初はstrの先頭にしといて、foundがNULLじゃなかったら pattern_len分進める
-	char *found;
-	while ((found = memmem(search_start, search_len, pattern, pattern_len)) != NULL) {
+	char *match_pos, *search_pos = buffer_start;
+	while((match_pos = memmem(search_pos, buffer_len - (search_pos - buffer_start), pattern, pattern_len)) != NULL) {
 		for (size_t i = 0; i < pattern_len; i++) {
-			found[i] = '*';
+			match_pos[i] = '*';
 		}
-		search_start += pattern_len;
+		search_pos = match_pos + pattern_len;
 	}
 
-	for (size_t i = 0; i < strlen(result); i++) {
-		write(1, &result[i], 1);
+	for (size_t i = 0; i < buffer_len; i++) {
+		write(1, &buffer_start[i], 1);
 	}
+	free(buffer_start);
 	return (0);
 }
